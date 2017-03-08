1c5d08cf55fd4c9229dd82d3777b7b8ff71d78e6
militoj <militoj@gmail.com>
2017-03-08 15:31:08 -0600

    implemented findByValue method

--- src/org/launchcode/techjobs/console/JobData.java
+++ src/org/launchcode/techjobs/console/JobData.java
@@ -10,6 +10,7 @@ import java.io.Reader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -82,6 +83,34 @@ public class JobData {
         }
 
         return jobs;
+    }
+
+
+    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+
+        // load data, if not already loaded
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (int i = 0; i < allJobs.size(); i++) {
+
+
+            for (Map.Entry<String, String> job : allJobs.get(i).entrySet()) {
+                if (job.getValue().contains(value)) {
+
+                    jobs.add(allJobs.get(i));
+                    i++;
+
+                }
+            }
+
+
+
+        }
+
+        return jobs;
+
     }
 
     /**
